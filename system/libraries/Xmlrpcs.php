ffa5356cba5ebfd60ba6d5b6df7a8ef2a2ad8a61
Ladybert <cyberrashad@gmail.com>
2024-07-04 13:50:21 +0700

    changes composer

--- system/libraries/Xmlrpcs.php
+++ system/libraries/Xmlrpcs.php
@@ -59,7 +59,6 @@ if ( ! class_exists('CI_Xmlrpc', FALSE))
  * @link		https://codeigniter.com/user_guide/libraries/xmlrpc.html
  */
 class CI_Xmlrpcs extends CI_Xmlrpc {
-
 	/**
 	 * Array of methods mapped to function names and signatures
 	 *
